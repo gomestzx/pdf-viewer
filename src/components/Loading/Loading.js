7686e26b612a460dee98d301067bbcf6b0c13fce
gomestzx <felipematheusdev@gmail.com>
2025-01-15 14:14:05 -0300

    fix: loading style

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -5,7 +5,7 @@ const Loading = ({ label }) => {
     <div className="flex flex-col justify-center items-center mt-4">
       <div className={styles.loader}></div>
       {label && (
-        <span className="mt-6 text-black font-semibold  font-raleway text-lg">
+        <span className="mt-6 text-black font-semibold  font-raleway text-lg w-[200px] text-center">
           {label}
         </span>
       )}
